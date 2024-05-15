36f9c9889620cb3875b95c04980ffd6fd835e3e0
slavikyd <slavikyd@gmail.com>
2024-05-15 09:43:28 +0300

    improper django import fix in test_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from dot_app.models import Board, Subtype, Manufacturer
-import data
+from . import data
 def create_model_tests(model_cls, attrs):
     class TestModel(TestCase):
         def test_positive_create(self):
