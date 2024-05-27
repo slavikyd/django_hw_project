a3fc3f6a6e87dfe284cb00d9db497b566fdefbfb
slavikyd <slavikyd@gmail.com>
2024-05-27 13:55:58 +0300

    subtypes page fix + minio tests fix

--- tests/test.sh
+++ tests/test.sh
@@ -4,11 +4,11 @@ export PG_PORT=5432
 export PG_USER=test
 export PG_PASSWORD=test
 export PG_DBNAME=test
-# export MINIO_ACCESS_KEY_ID=user
-# export MINIO_SECRET_ACCESS_KEY=password
-# export MINIO_STORAGE_BUCKET_NAME=static
-# export MINIO_API=http://localhost:9000
-# export MINIO_CONSISTENCY_CHECK_ON_START=False
+export MINIO_ACCESS_KEY_ID=user
+export MINIO_SECRET_ACCESS_KEY=password
+export MINIO_STORAGE_BUCKET_NAME=static
+export MINIO_API=http://localhost:9000
+export MINIO_CONSISTENCY_CHECK_ON_START=False
 
 
 
